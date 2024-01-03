421605c12c78950231cbd0ffee83e041d72afb54
Alma Nursultan <alma@Almas-Air.lan>
2024-01-03 12:34:48 -0700

    Updated info inside

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -2,5 +2,6 @@ public class SampleClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Im learning git");
+        System.out.println("updated my token today");
     }
 }
