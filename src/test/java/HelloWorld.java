09df48aa15a81a08d99f85cb86a0ed850b8490ba
Alma Nursultan <alma@Almas-Air.lan>
2024-01-04 19:06:59 -0700

    added a change in main method

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("It's thursday");
+        System.out.println("This is my second changeg");
     }
 }
+
