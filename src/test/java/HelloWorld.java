ce1f54cf5f055080c321719bb061770320653282
almanursultan <155504285+almanursultan@users.noreply.github.com>
2024-01-04 20:14:28 -0700

    Update HelloWorld.java
    changed day

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("It's thursday");
+        System.out.println("It's Friday tomorrow");
         System.out.println("This is my second changeg");
     }
     // this is the code which was added by another developer
